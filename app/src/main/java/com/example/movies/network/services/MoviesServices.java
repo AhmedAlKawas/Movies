9174cfbc5b8f25224d24001e007eeae41fbc609b
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2021-07-13 21:42:43 +0200

    Adding get popular people response model and adding the service

--- app/src/main/java/com/example/movies/network/services/MoviesServices.java
+++ app/src/main/java/com/example/movies/network/services/MoviesServices.java
@@ -1,4 +1,15 @@
 package com.example.movies.network.services;
 
+import com.example.movies.network.model.GetPopularMoviesResponse;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface MoviesServices {
+
+    @GET("movie/popular")
+    Observable<GetPopularMoviesResponse> getPopularMovies(@Query("page") int page,
+                                                          @Query("api_key") String apiKey);
+
 }
