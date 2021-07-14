0fe8d1254a8907be14ca244e82cbf6005e768f35
AhmedAlKawas <ahmed.alkawas77@gmail.com>
2021-07-14 17:09:17 +0200

    Adding pagging

--- app/src/main/java/com/example/movies/view/HomeActivity.java
+++ app/src/main/java/com/example/movies/view/HomeActivity.java
@@ -1,15 +1,18 @@
 package com.example.movies.view;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.movies.R;
 import com.example.movies.databinding.ActivityHomeBinding;
 import com.example.movies.interfaces.MovieCallback;
 import com.example.movies.network.model.GetPopularMoviesResponse;
 import com.example.movies.view.adapters.MoviesAdapter;
+import com.example.movies.view_model.MoviesViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,6 +23,8 @@ public class HomeActivity extends AppCompatActivity implements MovieCallback {
     private GetPopularMoviesResponse response;
     private List<GetPopularMoviesResponse.Result> movies;
     private MoviesAdapter moviesAdapter;
+    private int page = 1;
+    private MoviesViewModel moviesViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,27 @@ public class HomeActivity extends AppCompatActivity implements MovieCallback {
         homeBinding = DataBindingUtil.setContentView(HomeActivity.this,
                 R.layout.activity_home);
 
+        initListeners();
         getIntentExtras();
+
+    }
+
+    private void initListeners() {
+
+        moviesViewModel = new ViewModelProvider(HomeActivity.this)
+                .get(MoviesViewModel.class);
+        moviesViewModel.returnPopularMovies().observe(HomeActivity.this, paggingResponse -> {
+
+            homeBinding.lottie.setVisibility(View.GONE);
+
+            if (paggingResponse != null && paggingResponse.getResults().size() > 0) {
+
+                movies.addAll(paggingResponse.getResults());
+                moviesAdapter.notifyDataSetChanged();
+
+            }
+
+        });
 
     }
 
@@ -57,6 +82,13 @@ public class HomeActivity extends AppCompatActivity implements MovieCallback {
 
     @Override
     public void onLastMovieItemReached() {
+
+//        Check if we didn't reach the movies end
+        if (response.getResults().size() == 20) {
+            homeBinding.lottie.setVisibility(View.VISIBLE);
+            page++;
+            moviesViewModel.getPopularMovies(page, getString(R.string.api_key));
+        }
 
     }
 }
